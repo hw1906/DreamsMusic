1f4868f867b6a27b6d133589624ddc0ea3a24e23
DreamsRobot <hdhindihub@gmail.com>
2025-08-10 04:14:06 +0530

    Update extra.py

--- DreamsMusic/handlers/extra.py
+++ DreamsMusic/handlers/extra.py
@@ -1,1 +1,6 @@
-# Welcome messages & other extra handlers
+# handlers/extra.py
+# Welcome new users in groups
+
+async def welcome(client, message, lang):
+    for new_user in message.new_chat_members:
+        await message.reply(lang["welcome_message"].format(name=new_user.mention))
