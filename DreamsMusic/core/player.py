d8b2e350d0657bf8b883bcb105248abf4191fcc0
DreamsRobot <hdhindihub@gmail.com>
2025-08-10 04:11:49 +0530

    Update player.py

--- DreamsMusic/core/player.py
+++ DreamsMusic/core/player.py
@@ -1,1 +1,26 @@
-# Music player class and queue management
+# core/player.py
+# Music player & queue management logic
+
+class Player:
+    def __init__(self):
+        self.queue = []
+        self.is_playing = False
+
+    async def play(self, chat_id, song):
+        self.is_playing = True
+        # TODO: integrate PyTgCalls streaming here
+
+    async def pause(self):
+        self.is_playing = False
+
+    async def resume(self):
+        self.is_playing = True
+
+    async def stop(self):
+        self.is_playing = False
+        self.queue.clear()
+
+    async def skip(self):
+        pass
+
+player_instance = Player()
