74cb8eadc024baf4051608c6d7504075600e3783
DreamsRobot <hdhindihub@gmail.com>
2025-08-10 05:13:53 +0530

    Create settings.py

---
+++ DreamsMusic/handlers/settings.py
@@ -0,0 +1,17 @@
+from utils.language_util import get_chat_language
+
+async def play(client, message, ...):
+    chat_id = message.chat.id
+    lang = get_chat_language(chat_id)
+
+    # Load language dictionary depending on lang
+    texts = {
+        "no_query": {
+            "english": "Please provide a song name or URL to play.",
+            "hindi": "कृपया बजाने के लिए गाने का नाम या यूआरएल दें।"
+        },
+        # add more messages here
+    }
+
+    if no_query_condition:
+        await message.reply(texts["no_query"][lang])
