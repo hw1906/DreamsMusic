891c28264d13a0165ec2f00188fb59e7f433f485
DreamsRobot <hdhindihub@gmail.com>
2025-08-10 04:13:51 +0530

    Update broadcast.py

--- DreamsMusic/handlers/broadcast.py
+++ DreamsMusic/handlers/broadcast.py
@@ -1,1 +1,6 @@
-# Broadcast message handler
+# handlers/broadcast.py
+# Broadcast message to all users/chats (stub)
+
+async def broadcast(client, message):
+    # Implement broadcast logic
+    pass
