b94b8ba6b1c97d56cc4a8763e4ef35e465b807bb
hw1906 <148869343+hw1906@users.noreply.github.com>
2025-08-10 18:28:45 +0530

    Update language_util.py

--- DreamsMusic/utils/language_util.py
+++ DreamsMusic/utils/language_util.py
@@ -26,9 +26,7 @@ languages = {
         "no_query": "कृपया गाने का नाम या लिंक दें।",
         "playing": "अभी बज रहा है: {query}",
         "welcome_message": "{name} का समूह में स्वागत है!",
-        "stats_message": "अपटाइम: {uptime}
-उपयोगकर्ता: {users}
-चैट्स: {chats}",
+        "stats_message": "अपटाइम: {uptime}\nउपयोगकर्ता: {users}\nचैट्स: {chats}"
     }
 }
 
@@ -40,9 +38,3 @@ def get_chat_language(chat_id):
     """Get chat language setting, defaults to English if not set"""
     # TODO: Implement chat language storage/retrieval
     return 'en'  # Default to English for now
-```
-    }
-}
-
-def load_language(lang_code: str):
-    return languages.get(lang_code, languages["en"])
