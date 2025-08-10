7820273befcade35aa99a84658def714a37eda7e
hw1906 <148869343+hw1906@users.noreply.github.com>
2025-08-10 19:11:12 +0530

    Update start.py

--- DreamsMusic/handlers/start.py
+++ DreamsMusic/handlers/start.py
@@ -7,6 +7,6 @@ async def start(client, message, lang):
     text = lang["start_message"]
     buttons = [
         [InlineKeyboardButton("Help", callback_data="help")],
-        [InlineKeyboardButton("Support", url="https://t.me/DreamsSupport")]
+        [InlineKeyboardButton("Support", url="https://t.me/CloseFriendsCommunity")]
     ]
     await message.reply(text, reply_markup=InlineKeyboardMarkup(buttons))
